a365decc4ddaceeccdd185180e94dfd38e157990
Denis <den.brazhko@gmail.com>
2018-08-25 00:14:01 +0300

    frst

--- app/src/main/java/com/dendev/millionaire/MainActivity.java
+++ app/src/main/java/com/dendev/millionaire/MainActivity.java
@@ -2,12 +2,36 @@ package com.dendev.millionaire;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    //Buttons
+    Button newGameBut;
+
+
+    private void findID(){
+        newGameBut = findViewById(R.id.new_game_but);
+    }
+
+    private void setOnClick() {
+        newGameBut.setOnClickListener(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.enter_activity);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.new_game_but:
+
+                break;
+        }
     }
 }
